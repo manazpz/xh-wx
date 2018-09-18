77d33c7fab1466206987eff15056ab34ca0359e5
manazpz <1348319927@qq.com>
2018-09-18 23:20:36 +0800

    init

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -10,50 +10,50 @@
       </mt-navbar>
     </div>
 
-    <!--<mt-tab-container v-model="selected">-->
-      <!--<mt-tab-container-item id="sy">-->
-        <!--<mt-cell v-for="n in 10" :title="'content ' + n" />-->
-      <!--</mt-tab-container-item>-->
-      <!--<mt-tab-container-item id="js">-->
-        <!--<mt-cell v-for="n in 4" :title="'content ' + n" />-->
-      <!--</mt-tab-container-item>-->
-    <!--</mt-tab-container>-->
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="sy">
+        <mt-swipe :stopPropagation="true" :prevent="true" :auto="5000" class="banner" style="height: 200px;margin-top: 40px">
+          <mt-swipe-item v-for="item in banners">
+            <img :src="item.url" style="height: 100%;width: 100%">
+          </mt-swipe-item>
+        </mt-swipe>
+        <div class="home-change-purchase">
+          <ul>
+            <li>
+              <span>旧机</span>
+              <a>
+                <strong>iPhone 6/iPhone 6plus</strong>
+              </a>
+            </li>
+            <li>
+              <span>新机</span>
+              <a>
+                <strong>iPhone X/iPhone 8</strong>
+              </a>
+            </li>
+            <li>
+              <p>新人可领取200元大礼包</p>
+            </li>
+          </ul>
+          <mt-button size="large" class="exchange-btn">换购新机</mt-button>
+        </div>
+        <div class="h-prompt-img">
+          <h4>三天换新机</h4>
+          <div v-for="item in statements">
+            <a href="javascript:;" title="旧机估价">
+              <img :src="item.imgUrl">
+              <span>{{item.title}}</span>
+            </a>
+          </div>
+        </div>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="js">
+        <mt-cell v-for="n in 4" :title="'content ' + n" />
+      </mt-tab-container-item>
+    </mt-tab-container>
 
 
-    <mt-swipe :stopPropagation="true" :prevent="true" :auto="5000" class="banner" style="height: 200px;margin-top: 40px">
-      <mt-swipe-item v-for="item in banners">
-        <img :src="item.url" style="height: 100%;width: 100%">
-      </mt-swipe-item>
-    </mt-swipe>
-    <div class="home-change-purchase">
-      <ul>
-        <li>
-          <span>旧机</span>
-          <a>
-            <strong>iPhone 6/iPhone 6plus</strong>
-          </a>
-        </li>
-        <li>
-          <span>新机</span>
-          <a>
-            <strong>iPhone X/iPhone 8</strong>
-          </a>
-        </li>
-        <li>
-          <p>新人可领取200元大礼包</p>
-        </li>
-      </ul>
-      <mt-button size="large" class="exchange-btn">换购新机</mt-button>
-    </div>
-    <div class="h-prompt-img">
-      <h4>三天换新机</h4>
-      <div v-for="item in statements">
-        <a href="javascript:;" title="旧机估价">
-            <img :src="item.imgUrl">
-            <span>{{item.title}}</span>
-        </a>
-      </div>
-		</div>
+
   </div>
 </template>
 
